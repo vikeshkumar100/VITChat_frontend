b3de5fc796a61dd77c7c9dac90e2dc30572f7568
Vikesh <vikeshkapoor100@gmail.com>
2025-03-17 14:48:30 +0530

    add : features section in homepage

---
+++ src/components/Card/FeaturesCard.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { MagicCard } from "../../components/magicui/magic-card";
+import { Link } from "react-router-dom";
+const FeaturesCard = ({ route, icon, title, description }) => {
+  return (
+    <Link to={route}>
+      <MagicCard className="rounded-2xl">
+        <div className="h-[300px] min-w-80 flex flex-col justify-center gap-6 p-4">
+          <h3 className="text-4xl font-bold">
+            {title}
+            {icon}
+          </h3>
+          <p className="text-lg text-gray-500">{description}</p>
+        </div>
+      </MagicCard>
+    </Link>
+  );
+};
+
+export default FeaturesCard;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,25 +4,28 @@ import { RainbowButton } from "../../components/magicui/rainbow-button";
 import { TypingAnimation } from "../../components/magicui/typing-animation";
 import { VelocityScroll } from "../../components/magicui/scroll-based-velocity";
 import { Link } from "react-router-dom";
+import FeaturesCard from "@/components/Card/FeaturesCard";
 
 const Home = () => {
   return (
     <div className="relative min-h-screen max-w-full flex flex-col items-center justify-center">
       {/* background  */}
-      <div className="absolute inset-0 -z-10">
+      <div className="absolute h-screen inset-0 -z-10">
         <AnimatedGridPatternDemo />
       </div>
+      {/* blue gradient bg */}
+      <div className="absolute top-1/3 w-full h-32 bg-blue-400 rounded-full opacity-50 dark:opacity-30 blur-[180px]"></div>
 
       {/*hero section */}
-      <div className="md:text-4xl  text-xl min-h-screen max-w-full text-center flex flex-col items-center justify-center gap-5 p-8">
+      <div className="lg:text-5xl md:text-4xl text-xl min-h-screen max-w-full text-center flex flex-col items-center justify-center gap-7 p-8 lg:px-24 md:pt-20">
         <h2>
-          <TypingAnimation className="md:text-5xl">
+          <TypingAnimation className="md:text-5xl text-3xl">
             Welcome to VITChat!
           </TypingAnimation>
         </h2>
         <h1 className="font-bold">
           Connect, Chat, and Collaborate – The Ultimate Chat Platform for
-          VITians!
+          <span className="text-blue-400"> VITians!</span>
         </h1>
         <h4 className="text-lg text-gray-500 md:w-2/3 backdrop-blur-sm p-2">
           VITChat is a real-time chat platform designed exclusively for VIT
@@ -34,13 +37,46 @@ const Home = () => {
         </Link>
       </div>
 
+      {/* marquee  */}
       <div className="flex w-full flex-col items-center justify-center overflow-hidden">
         <VelocityScroll defaultVelocity={1}>
           🌍Global Chat • 💬Random Chat • 🤖AI Chatbot • 🔒Secure VIT Email
           Login • ⚡Real-Time Messaging
         </VelocityScroll>
       </div>
-      <div className="absolute bottom-36 w-full h-32 bg-blue-400 rounded-full opacity-100 dark:opacity-30 blur-[180px]"></div>
+
+      {/* features */}
+      <div className="min-h-screen w-full flex flex-col items-center justify-top pt-40 gap-28">
+        <h2 className="text-3xl md:text-6xl font-bold bg-blue-600 p-4 w-full">
+        Why VITChat? – Features You’ll Love
+        </h2>
+        <div className="w-2/3 flex flex-col md:flex-row items-center justify-center gap-10">
+          <FeaturesCard
+            route="/chat/global-chat"
+            icon="🌍"
+            title="Global Chat"
+            description="Join discussions with VIT students across all campuses and stay connected."
+          />
+          <FeaturesCard
+            route="/chat/random-chat"
+            icon="💬"
+            title="Random Chat"
+            description="Chat with random students and make new friends. You never know who you might meet!"
+          />
+          <FeaturesCard
+            route="/chat/chat-bot"
+            icon="🤖"
+            title="AI Chatbot"
+            description="Get instant answers to your queries from our AI chatbot."
+          />
+          <FeaturesCard
+            route="/chat"
+            icon="🛡️"
+            title="Privacy & Security"
+            description="Exclusive to VIT students. Your messages are end-to-end secure, and we do not store them."
+          />
+        </div>
+      </div>
     </div>
   );
 };
